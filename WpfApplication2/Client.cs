fbfc3d93730cde8fb1e02fbb8bfae2793ab0df42
IÑAKI CABO ALONSO <icaboalo@itam.mx>
2018-04-02 12:46:15 -0500

    Initial commit

---
+++ WpfApplication2/Client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication2 {
+
+    class Client {
+
+        int idClient;
+        String name;
+        String lastname;
+        String country;
+
+        public Client(int idClient, String name, String lastname, String country) {
+            this.idClient = idClient;
+            this.name = name;
+            this.lastname = lastname;
+            this.country = country;
+        }
+
+        public static List<Client> getClientsByCountry(String country) {
+            List<Client> list = new List<Client>();
+            SqlConnection con = Connection.addConnection();
+
+            if (con != null) {
+                SqlCommand cmd = new SqlCommand(String.Format("SELECT * FROM cliente WHERE pais = '{0}'", country), con);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read()) {
+                    list.Add(new Client(reader.GetInt16(0), reader.GetString(1), reader.GetString(2), reader.GetString(3)));
+                }
+            }
+
+            return list;
+        }
+
+        
+    }
+}
---
+++ WpfApplication2/Connection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApplication2 {
+    class Connection {
+        public static SqlConnection addConnection() {
+            SqlConnection con = new SqlConnection("");
+
+            try {
+                con.Open();
+            } catch {
+                MessageBox.Show("No se pudo conectar...");
+                con = null;
+            }
+
+            return con;
+        }
+
+        public static void loadCountries(ComboBox cb) {
+            SqlConnection con = addConnection();
+            if (con != null) {
+                SqlCommand cmd = new SqlCommand("SELECT DISTINCT pais FROM cliente;", con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read()) {
+                    cb.Items.Add(reader.GetString(0));
+                }
+                con.Close();
+            }
+        }
+    }
+}
